e27a7d93a679f058ad96f42c1cd9b92a14accdee
Héctor Cabrera <hcabrerab@gmail.com>
2022-08-07 12:10:08 -0400

    Output: replaces strip_tags() calls with wp_kses()

--- src/Output.php
+++ src/Output.php
@@ -671,9 +671,19 @@ class Output {
 
             // remove HTML tags if requested
             if ( $this->options['post-excerpt']['keep_format'] ) {
-                $excerpt = strip_tags($excerpt, '<a><b><i><em><strong>');
+                $excerpt = wp_kses(
+                    $excerpt,
+                    [
+                        'a' => [
+                            'href' => [],
+                            'title' => []
+                        ],
+                        'em' => [],
+                        'strong' => []
+                    ]
+                );
             } else {
-                $excerpt = strip_tags($excerpt);
+                $excerpt = wp_kses($excerpt, []);
 
                 // remove URLs, too
                 $excerpt = preg_replace('_^(?:(?:https?|ftp)://)(?:\S+(?::\S*)?@)?(?:(?!10(?:\.\d{1,3}){3})(?!127(?:\.\d{1,3}){3})(?!169\.254(?:\.\d{1,3}){2})(?!192\.168(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\x{00a1}-\x{ffff}0-9]+-?)*[a-z\x{00a1}-\x{ffff}0-9]+)(?:\.(?:[a-z\x{00a1}-\x{ffff}0-9]+-?)*[a-z\x{00a1}-\x{ffff}0-9]+)*(?:\.(?:[a-z\x{00a1}-\x{ffff}]{2,})))(?::\d{2,5})?(?:/[^\s]*)?$_iuS', '', $excerpt);
